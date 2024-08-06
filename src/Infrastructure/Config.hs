b990cfac3a5f046146137b2b5b3b526d36354c82
Λ <sobakavosne@gmail.com>
2024-08-06 03:41:10 +0200

    fix(src): extend configuration

--- src/Infrastructure/Config.hs
+++ src/Infrastructure/Config.hs
@@ -6,20 +6,29 @@ module Infrastructure.Config
   ( loadBoltCfg
   ) where
 
-import qualified Configuration.Dotenv as Dotenv
-import           Data.Default         (def)
-import           Data.Maybe           (fromMaybe)
-import           Data.Text            (pack, unpack)
-import           Database.Bolt        (BoltCfg (..))
-import           System.Environment   (lookupEnv)
+import           Data.Default       (def)
+import           Data.Maybe         (fromMaybe)
+import           Data.Text          (pack, unpack)
+import           Database.Bolt      (BoltCfg (..))
+import           System.Environment (lookupEnv)
 
 loadBoltCfg :: IO BoltCfg
 loadBoltCfg = do
-  Dotenv.loadFile Dotenv.defaultConfig
   user <- pack . fromMaybe (unpack $ user def) <$> lookupEnv "NEO4J_USER"
   pwd <- pack . fromMaybe (unpack $ password def) <$> lookupEnv "NEO4J_PASSWORD"
   host <- fromMaybe (host def) <$> lookupEnv "NEO4J_HOST"
   port <- read . fromMaybe (show $ port def) <$> lookupEnv "NEO4J_BOLT_PORT"
   return
     BoltCfg
-      {user, password = pwd, host, port, socketTimeout = socketTimeout def}
+      { user
+      , password = pwd
+      , host
+      , port
+      , socketTimeout = socketTimeout def
+      , magic = magic def
+      , version = version def
+      , userAgent = userAgent def
+      , maxChunkSize = maxChunkSize def
+      , authType = authType def
+      , secure = secure def
+      }

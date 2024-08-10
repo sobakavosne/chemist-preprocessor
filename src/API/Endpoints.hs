3d74231a7ac57903857672e81eaa5a4f85ed0844
mr. dima <sobakavosne@gmail.com>
2024-08-10 03:55:13 +0200

    fix(neo4j): move from `latest` to `4.1` (#4)
    * fix(migrations): create conditional transactions & add migration version check
    
    * feat(src): add logger
    
    * feat(root): add `Issues`
    
    * feat(src): add logger
    
    * fix(docker): fix migration & fix healthcheck & fix Docker network

--- src/API/Endpoints.hs
+++ src/API/Endpoints.hs
@@ -7,24 +7,52 @@ module API.Endpoints
   , server
   ) where
 
-import           Control.Monad.IO.Class  (MonadIO (liftIO))
-import           Data.Bool               (bool)
-import           DataTypes               (HealthCheck (..))
-import           Infrastructure.Config   (loadBoltCfg)
-import           Infrastructure.Database (checkNeo4j, withNeo4j)
-import qualified Servant                 as S
+import           Control.Monad.IO.Class          (MonadIO (liftIO))
+import           Data.Bool                       (bool)
+import qualified Data.ByteString.Char8           as BS
+import qualified Data.ByteString.Lazy.Char8      as LBS
+import           Data.Time                       (getCurrentTime)
+import           DataTypes                       (HealthCheck (..))
+import           Infrastructure.Config           (loadBoltCfg)
+import           Infrastructure.Database         (checkNeo4j, withNeo4j)
+import           Network.HTTP.Types.Header       (hContentType)
+import qualified Servant                         as S
+import           System.Log.FastLogger           (ToLogStr (toLogStr),
+                                                  defaultBufSize, flushLogStr,
+                                                  pushLogStrLn)
+import           System.Log.FastLogger.LoggerSet (LoggerSet, newStdoutLoggerSet)
 
-type API = "health" S.:> S.Get '[ S.JSON] HealthCheck
+type API
+   = "health" S.:> S.Get '[ S.JSON] (S.Headers '[ S.Header "Content-Type" String] HealthCheck)
 
 api :: S.Proxy API
 api = S.Proxy
 
-healthHandler :: S.Handler HealthCheck
+initLogger :: IO LoggerSet
+initLogger = newStdoutLoggerSet defaultBufSize
+
+logInfo :: LoggerSet -> String -> IO ()
+logInfo logger msg = do
+  timestamp <- getCurrentTime
+  (pushLogStrLn logger . toLogStr) (unwords [show timestamp, "-", msg])
+  flushLogStr logger
+
+healthHandler ::
+     S.Handler (S.Headers '[ S.Header "Content-Type" String] HealthCheck)
 healthHandler = do
+  logger <- liftIO initLogger
   boltCfg <- liftIO loadBoltCfg
-  neo4jStatus <- liftIO $ withNeo4j boltCfg checkNeo4j
+  neo4jStatus <- (liftIO . withNeo4j boltCfg) checkNeo4j
   let neo4jMessage = bool "Neo4j is down" "Neo4j is alive" neo4jStatus
-  return HealthCheck {status = "Server is alive", neo4j = neo4jMessage}
+  let health = HealthCheck {status = "Server is alive", neo4j = neo4jMessage}
+  (liftIO . logInfo logger . show) health
+  if neo4jStatus
+    then return $ S.addHeader "application/json" health
+    else S.throwError
+           S.err500
+             { S.errBody = LBS.pack "Neo4j is down"
+             , S.errHeaders = [(hContentType, BS.pack "application/json")]
+             }
 
 server :: S.Server API
 server = healthHandler

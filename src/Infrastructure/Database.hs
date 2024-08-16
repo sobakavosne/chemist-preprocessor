7ff50f8a0866270826f86a744a7f6a550a882c5f
Λ <sobakavosne@gmail.com>
2024-08-16 02:49:01 +0200

    feat(src): add tested untyped bad `getReaction` & add `RecordValue` instances

--- src/Infrastructure/Database.hs
+++ src/Infrastructure/Database.hs
@@ -1,13 +1,24 @@
+{-# LANGUAGE NamedFieldPuns      #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Infrastructure.Database
   ( withNeo4j
   , checkNeo4j
+  , getReaction
   ) where
 
 import           Control.Monad.Except   (MonadError (catchError))
 import           Control.Monad.IO.Class (MonadIO)
 import           Data.Text              (pack)
 import           Database.Bolt          (BoltActionT, BoltCfg, BoltError,
-                                         connect, query, run)
+                                         Value (I), connect, props, query,
+                                         queryP, run)
+import           Helpers                (unrecord)
+import           Models                 (ACCELERATE, Catalyst, Molecule,
+                                         PRODUCT_FROM, REAGENT_IN,
+                                         Reaction (..), ReactionDetails (..))
+import           Prelude                hiding (id)
 
 withNeo4j :: MonadIO m => BoltCfg -> BoltActionT m b -> m b
 withNeo4j cfg action = do
@@ -21,12 +32,27 @@ checkNeo4j = do
   where
     handleException :: BoltError -> BoltActionT IO [a]
     handleException _ = return []
--- initializeDatabase :: BoltActionT IO ()
--- initializeDatabase = do
---   _ <- queryP "CREATE INDEX ON :Molecule(id)" empty
---   _ <- queryP "CREATE INDEX ON :Reaction(id)" empty
---   _ <- queryP "CREATE INDEX ON :Catalyst(id)" empty
---   return ()
+
+getReaction :: Int -> BoltActionT IO ReactionDetails
+getReaction id = do
+  result <-
+    queryP
+      "MATCH \
+             \(reaction:Reaction { id: $id })<-[accelerate:ACCELERATE]-(catalyst:Catalyst), \
+             \(reaction)-[product_from:PRODUCT_FROM]->(product:Molecule), \
+             \(reaction)<-[reagent_in:REAGENT_IN]-(reagent:Molecule) \
+             \RETURN reaction, accelerate, catalyst, product, reagent, product_from, reagent_in"
+      (props [("id", I id)])
+  (reaction :: Reaction) <- head <$> unrecord result "reaction"
+  (reagents :: [Molecule]) <- unrecord result "reagent"
+  (products :: [Molecule]) <- unrecord result "product"
+  (inbound :: [REAGENT_IN]) <- unrecord result "reagent_in"
+  (outbound :: [PRODUCT_FROM]) <- unrecord result "product_from"
+  (accelerate :: [ACCELERATE]) <- unrecord result "accelerate"
+  (catalysts :: [Catalyst]) <- unrecord result "catalyst"
+  return $
+    ReactionDetails
+      {reaction, reagents, products, inbound, outbound, accelerate, catalysts}
 -- createMolecule :: MonadIO m => Molecule -> BoltActionT m [Record]
 -- createMolecule Molecule {id, smiles, iupacName} = do
 --   let params =

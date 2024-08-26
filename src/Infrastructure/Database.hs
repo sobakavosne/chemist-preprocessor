d79c5755c913961146f5cdc91119eb9f7a565f57
Λ <sobakavosne@gmail.com>
2024-08-26 03:08:54 +0200

    fix(Infrastructure): flip arguments in `withNeo4j` & change fnc name & use general Bolt types

--- src/Infrastructure/Database.hs
+++ src/Infrastructure/Database.hs
@@ -5,23 +5,21 @@
 module Infrastructure.Database
   ( withNeo4j
   , checkNeo4j
-  , getReaction
+  , fetchReaction
   ) where
 
 import           Control.Monad.Except   (MonadError (catchError))
 import           Control.Monad.IO.Class (MonadIO)
 import           Data.Text              (pack)
-import           Database.Bolt          (BoltActionT, BoltCfg, BoltError,
-                                         Value (I), connect, props, query,
-                                         queryP, run)
+import           Database.Bolt          (BoltActionT, BoltCfg, BoltError, Node,
+                                         Relationship, Value (I), connect,
+                                         props, query, queryP, run)
 import           Helpers                (unrecord)
-import           Models                 (ACCELERATE, Catalyst, Molecule,
-                                         PRODUCT_FROM, REAGENT_IN,
-                                         Reaction (..), ReactionDetails (..))
+import           Models                 (RawReactionDetails (..))
 import           Prelude                hiding (id)
 
-withNeo4j :: MonadIO m => BoltCfg -> BoltActionT m b -> m b
-withNeo4j cfg action = do
+withNeo4j :: MonadIO m => BoltActionT m b -> BoltCfg -> m b
+withNeo4j action cfg = do
   pipe <- connect cfg
   run pipe action
 
@@ -33,25 +31,32 @@ checkNeo4j = do
     handleException :: BoltError -> BoltActionT IO [a]
     handleException _ = return []
 
-getReaction :: Int -> BoltActionT IO ReactionDetails
-getReaction id = do
+fetchReaction :: Int -> BoltActionT IO RawReactionDetails
+fetchReaction id = do
   result <-
     queryP
       "MATCH \
              \(reaction:Reaction { id: $id })<-[accelerate:ACCELERATE]-(catalyst:Catalyst), \
              \(reaction)-[product_from:PRODUCT_FROM]->(product:Molecule), \
              \(reaction)<-[reagent_in:REAGENT_IN]-(reagent:Molecule) \
-             \RETURN reaction, accelerate, catalyst, product, reagent, product_from, reagent_in"
+             \RETURN DISTINCT reaction, \
+             \  COLLECT(DISTINCT accelerate) AS accelerates, \
+             \  COLLECT(DISTINCT catalyst) AS catalysts, \
+             \  COLLECT(DISTINCT product) AS products, \
+             \  COLLECT(DISTINCT reagent) AS reagents, \
+             \  COLLECT(DISTINCT product_from) AS product_froms, \
+             \  COLLECT(DISTINCT reagent_in) AS reagent_ins"
       (props [("id", I id)])
-  (reaction :: Reaction) <- head <$> unrecord result "reaction"
-  (reagents :: [Molecule]) <- unrecord result "reagent"
-  (products :: [Molecule]) <- unrecord result "product"
-  (inbound :: [REAGENT_IN]) <- unrecord result "reagent_in"
-  (outbound :: [PRODUCT_FROM]) <- unrecord result "product_from"
-  (accelerate :: [ACCELERATE]) <- unrecord result "accelerate"
-  (catalysts :: [Catalyst]) <- unrecord result "catalyst"
-  return $
-    ReactionDetails
+  (reaction :: Node) <- head <$> unrecord result "reaction"
+  -- (reaction2 :: Either ParsingError Reaction) <- (lift . runExceptT . exact) . head =<< unrecord result "reaction"
+  (reagents :: [Node]) <- head <$> unrecord result "reagents"
+  (inbound :: [Relationship]) <- head <$> unrecord result "reagent_ins"
+  (products :: [Node]) <- head <$> unrecord result "products"
+  (outbound :: [Relationship]) <- head <$> unrecord result "product_froms"
+  (accelerate :: [Relationship]) <- head <$> unrecord result "accelerates"
+  (catalysts :: [Node]) <- head <$> unrecord result "catalysts"
+  return
+    RawDetails
       {reaction, reagents, products, inbound, outbound, accelerate, catalysts}
 -- createMolecule :: MonadIO m => Molecule -> BoltActionT m [Record]
 -- createMolecule Molecule {id, smiles, iupacName} = do

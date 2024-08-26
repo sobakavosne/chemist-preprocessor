cb4c53d04a938361059da5418678f0592613af81
Λ <sobakavosne@gmail.com>
2024-08-26 03:00:02 +0200

    feat(Domain): init `Service`

---
+++ src/Domain/Helpers.hs
@@ -0,0 +1,1 @@
+module Domain.Helpers where
--- src/Domain/Service.hs
+++ src/Domain/Service.hs
@@ -1,1 +1,12 @@
 module Domain.Service where
+
+import           Control.Monad.IO.Class  (MonadIO (liftIO))
+import           Infrastructure.Config   (loadBoltCfg)
+import           Infrastructure.Database (fetchReaction, withNeo4j)
+import           Infrastructure.Type     (convert)
+import           Models                  (ReactionDetails)
+import           Prelude                 hiding (id)
+
+getReaction :: Int -> IO ReactionDetails
+getReaction id =
+  convert <$> (liftIO . withNeo4j (fetchReaction id) =<< liftIO loadBoltCfg)

8ce745711c3ce18ffe4fa8da7a7bbe9348ebfab2
Λ <sobakavosne@gmail.com>
2024-08-26 03:02:46 +0200

    feat(src): remove unused & introduce `RawReactionDetails`

--- src/Models.hs
+++ src/Models.hs
@@ -1,9 +1,8 @@
 {-# LANGUAGE DeriveGeneric         #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE InstanceSigs          #-}
-{-# LANGUAGE NamedFieldPuns        #-}
-{-# LANGUAGE OverloadedStrings     #-}
 {-# LANGUAGE ScopedTypeVariables   #-}
+{-# OPTIONS_GHC -Wno-missing-fields #-}
 
 module Models
   ( Molecule(..)
@@ -13,18 +12,25 @@ module Models
   , REAGENT_IN(..)
   , ACCELERATE(..)
   , ReactionDetails(..)
+  , RawReactionDetails(..)
+  -- , Interactant(..)
   ) where
 
 import           Data.Aeson    (FromJSON, ToJSON)
 
 import           Data.Default  (Default (def))
-import           Data.Text     (unpack)
-import           Database.Bolt (RecordValue (exactEither), UnpackError (Not),
-                                Value (N, S))
+import           Database.Bolt (Node, Relationship)
 import           GHC.Generics  (Generic)
-import           Helpers       (exactFields, fromDouble)
 import           Prelude       hiding (id)
 
+-- data Interactant
+--   = IMolecule Molecule
+--   | IReaction Reaction
+--   | ICatalyst Catalyst
+--   | IPRODUCT_FROM PRODUCT_FROM
+--   | IREAGENT_IN REAGENT_IN
+--   | IACCELERATE ACCELERATE
+--   deriving (Eq, Show, Generic)
 data Molecule =
   Molecule
     { id        :: Int
@@ -37,15 +43,6 @@ instance FromJSON Molecule
 
 instance ToJSON Molecule
 
-instance RecordValue Molecule where
-  exactEither :: Value -> Either UnpackError Molecule
-  exactEither (S structure) = do
-    id <- exactFields "id" structure
-    smiles <- unpack <$> exactFields "smiles" structure
-    iupacName <- unpack <$> exactFields "iupacName" structure
-    return Molecule {id, smiles, iupacName}
-  exactEither _ = Left $ Not "Molecule"
-
 data Reaction =
   Reaction
     { id   :: Int
@@ -56,14 +53,6 @@ data Reaction =
 instance FromJSON Reaction
 
 instance ToJSON Reaction
-
-instance RecordValue Reaction where
-  exactEither :: Value -> Either UnpackError Reaction
-  exactEither (S structure) = do
-    id <- exactFields "id" structure
-    name <- unpack <$> exactFields "name" structure
-    return Reaction {id, name}
-  exactEither _ = Left $ Not "Reaction"
 
 data Catalyst =
   Catalyst
@@ -77,15 +66,6 @@ instance FromJSON Catalyst
 
 instance ToJSON Catalyst
 
-instance RecordValue Catalyst where
-  exactEither :: Value -> Either UnpackError Catalyst
-  exactEither (S structure) = do
-    id <- exactFields "id" structure
-    smiles <- unpack <$> exactFields "smiles" structure
-    name <- unpack <$> exactFields "name" structure
-    return Catalyst {id, smiles, name}
-  exactEither _ = Left $ Not "Catalyst"
-
 newtype PRODUCT_FROM =
   PRODUCT_FROM
     { amount :: Float
@@ -95,13 +75,6 @@ newtype PRODUCT_FROM =
 instance FromJSON PRODUCT_FROM
 
 instance ToJSON PRODUCT_FROM
-
-instance RecordValue PRODUCT_FROM where
-  exactEither :: Value -> Either UnpackError PRODUCT_FROM
-  exactEither (S structure) = do
-    amount <- fromDouble <$> exactFields "amount" structure
-    return PRODUCT_FROM {amount}
-  exactEither _ = Left $ Not "PRODUCT_FROM"
 
 data ACCELERATE
   -- | `def` - the default ACCELERATE value corresponds to Standard Temperature and Pressure (STP):
@@ -122,15 +95,6 @@ instance FromJSON ACCELERATE
 
 instance ToJSON ACCELERATE
 
-instance RecordValue ACCELERATE where
-  exactEither :: Value -> Either UnpackError ACCELERATE
-  exactEither (S structure) = do
-    temperature <- map fromDouble <$> exactFields "temperature" structure
-    pressure <- map fromDouble <$> exactFields "pressure" structure
-    return ACCELERATE {temperature = temperature, pressure}
-  exactEither (N _) = Right def
-  exactEither _ = Left $ Not "ACCELERATE"
-
 newtype REAGENT_IN =
   REAGENT_IN
     { amount :: Float
@@ -141,25 +105,27 @@ instance FromJSON REAGENT_IN
 
 instance ToJSON REAGENT_IN
 
-instance RecordValue REAGENT_IN where
-  exactEither :: Value -> Either UnpackError REAGENT_IN
-  exactEither (S structure) = do
-    amount <- fromDouble <$> exactFields "amount" structure
-    return REAGENT_IN {amount}
-  exactEither _ = Left $ Not "REAGENT_IN"
-
 data ReactionDetails =
-  ReactionDetails
-    { reaction   :: Reaction
-    , reagents   :: [Molecule]
-    , products   :: [Molecule]
-    , inbound    :: [REAGENT_IN]
-    , outbound   :: [PRODUCT_FROM]
-    , accelerate :: [ACCELERATE] -- list of all acceleration options
-    , catalysts  :: [Catalyst]
+  Details
+    { reaction         :: Reaction
+    , inboundReagents  :: [(Molecule, REAGENT_IN)]
+    , outboundProducts :: [(Molecule, PRODUCT_FROM)]
+    , conditions       :: [(Catalyst, ACCELERATE)]
     }
   deriving (Show, Generic)
 
 instance FromJSON ReactionDetails
 
 instance ToJSON ReactionDetails
+
+data RawReactionDetails =
+  RawDetails
+    { reaction   :: Node
+    , reagents   :: [Node]
+    , products   :: [Node]
+    , inbound    :: [Relationship]
+    , outbound   :: [Relationship]
+    , accelerate :: [Relationship]
+    , catalysts  :: [Node]
+    }
+  deriving (Show, Eq)

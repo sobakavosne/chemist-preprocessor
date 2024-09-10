95cc21bdb6def774ef2418048394aacecd8279e7
mr. dima <sobakavosne@gmail.com>
2024-09-10 03:12:54 +0200

    feat(test): add `Mechanism` test case

--- test/Test.hs
+++ test/Test.hs
@@ -2,16 +2,18 @@ module Main
   ( main
   ) where
 
-import           Domain.Converter.Units.ToDetailsSpec    (toDetailsSpec)
-import           Domain.Converter.Units.ToPathSpec       (toPathSpec)
-import           Domain.Converter.Units.ToRawDetailsSpec (toRawDetailsSpec)
-import           Domain.Converter.Units.ToReactionSpec   (toReactionSpec)
-import           Test.Hspec                              (describe, hspec)
+import           Domain.Converter.Units.ToMechanismDetailsSpec (toMechanismDetailsSpec)
+import           Domain.Converter.Units.ToPathSpec             (toPathSpec)
+import           Domain.Converter.Units.ToRawDetailsSpec       (toRawDetailsSpec)
+import           Domain.Converter.Units.ToReactionDetailsSpec  (toReactionDetailsSpec)
+import           Domain.Converter.Units.ToReactionSpec         (toReactionSpec)
+import           Test.Hspec                                    (describe, hspec)
 
 main :: IO ()
 main =
   hspec $ do
-    describe "Domain.Converter.Units.ToDetails" toDetailsSpec
+    describe "Domain.Converter.Units.ToMechanismDetails" toMechanismDetailsSpec
     describe "Domain.Converter.Units.ToPath" toPathSpec
     describe "Domain.Converter.Units.ToRawDetails" toRawDetailsSpec
+    describe "Domain.Converter.Units.ToReactionDetails" toReactionDetailsSpec
     describe "Domain.Converter.Units.ToReaction" toReactionSpec

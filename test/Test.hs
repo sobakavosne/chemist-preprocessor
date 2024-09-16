fd023c301844f6992cd59deb997604564c0a476e
mr. dima <sobakavosne@gmail.com>
2024-09-16 04:31:50 +0200

    feat(test): fix test paths & fix names

--- test/Test.hs
+++ test/Test.hs
@@ -2,18 +2,19 @@ module Main
   ( main
   ) where
 
-import           Domain.Converter.Units.ToMechanismDetailsSpec (toMechanismDetailsSpec)
-import           Domain.Converter.Units.ToPathSpec             (toPathSpec)
-import           Domain.Converter.Units.ToRawDetailsSpec       (toRawDetailsSpec)
-import           Domain.Converter.Units.ToReactionDetailsSpec  (toReactionDetailsSpec)
-import           Domain.Converter.Units.ToReactionSpec         (toReactionSpec)
-import           Test.Hspec                                    (describe, hspec)
+import           Domain.Converter.Units.ToMechanismDetailsSpec   (toMechanismDetailsSpec)
+import           Domain.Converter.Units.ToPathSpec               (toPathSpec)
+import           Domain.Converter.Units.ToRawReactionDetailsSpec (toRawReactionDetailsSpec)
+import           Domain.Converter.Units.ToReactionDetailsSpec    (toReactionDetailsSpec)
+import           Domain.Converter.Units.ToReactionSpec           (toReactionSpec)
+import           Test.Hspec                                      (describe,
+                                                                  hspec)
 
 main :: IO ()
 main =
   hspec $ do
     describe "Domain.Converter.Units.ToMechanismDetails" toMechanismDetailsSpec
     describe "Domain.Converter.Units.ToPath" toPathSpec
-    describe "Domain.Converter.Units.ToRawDetails" toRawDetailsSpec
+    describe "Domain.Converter.Units.ToRawReactionDetails" toRawReactionDetailsSpec
     describe "Domain.Converter.Units.ToReactionDetails" toReactionDetailsSpec
     describe "Domain.Converter.Units.ToReaction" toReactionSpec
